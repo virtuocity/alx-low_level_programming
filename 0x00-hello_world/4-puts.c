53eb305805ab1de8046c938e2ccb22a7f7d49ab6
gabaya <gabayaconsult@gmail.com>
2022-09-08 23:17:19 +0000

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
 int main(void)
 
 {
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+	/**
+ 	* main - Entry point
+	*
+	* Return: Always 0 (Success)
+	*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
