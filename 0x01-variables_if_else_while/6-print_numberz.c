5eb3fa4975e916a0b28d9573585f89b79050d883
gabaya <gabayaconsult@gmail.com>
2022-09-09 23:38:03 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	 int i;
+	/*For loop to print numbers*/
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
