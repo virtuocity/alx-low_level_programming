aa64247dd7c93952925af9ac8bf7954617e4fda8
gabaya <gabayaconsult@gmail.com>
2022-09-09 22:31:31 +0000

    Lower and Upper alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char lowera;
+	char uppera;
+
+	lowera = 'a';
+	/* While loop to print upper case alphabet*/
+	while (lowera <= 'z')
+	{
+		putchar(lowera);
+		lowera++;
+	}
+	putchar('\n');
+	uppera = 'A';
+	/*While loop to print upper case*/
+	while (uppera <= 'Z')
+	{
+		putchar(uppera);
+		uppera++;
+	}
+	putchar('\n');
+
+	return (0);
+}
