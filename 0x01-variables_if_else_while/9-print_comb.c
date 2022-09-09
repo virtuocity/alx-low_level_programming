a082c76fe5ce054392c8de0bdf233fda996f556c
gabaya <gabayaconsult@gmail.com>
2022-09-09 23:49:30 +0000

    Print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	 int i;
+	/*For loop to print numbers*/
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
