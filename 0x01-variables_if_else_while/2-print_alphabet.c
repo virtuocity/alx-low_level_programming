4232710efa7671ad80419576d1c60ec04d816f44
gabaya <gabayaconsult@gmail.com>
2022-09-09 22:24:17 +0000

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+	/* While loop to print alphabet*/
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
