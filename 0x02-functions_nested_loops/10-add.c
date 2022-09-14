c1eff651fc7e0e8ab22840cb3812f9a9d10ec028
gabaya <gabayaconsult@gmail.com>
2022-09-14 09:42:39 +0000

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add- add two integers
+ *
+ * @arg1: operans one
+ * @arg2: operand two
+ * Return: sum of the two operands
+ */
+ int add (int i,int j)
+ {
+	 int sum;
+	 sum = i + j;
+	 return (sum);
+ }
+
