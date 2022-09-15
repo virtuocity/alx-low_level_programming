9e3979e1d80ead77e0e647e80e7ae213c095e60f
gabaya <gabayaconsult@gmail.com>
2022-09-15 01:48:51 +0000

    Multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
 				product = i*j;
 				if (j < n)
 				{
-					printf("%4d,\t", product);
+					printf("%4d,", product);
 				}
 				else
 					printf("%4d", product);
