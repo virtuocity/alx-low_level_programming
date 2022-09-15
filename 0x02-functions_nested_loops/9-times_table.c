a5fbf91a19858f6afb70f6b9fccea90bee8d6f46
gabaya <gabayaconsult@gmail.com>
2022-09-15 14:15:35 +0000

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,18 +7,18 @@
  */
 void times_table(void)
 {
-	int i,j,product;
+	int i, j, product;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0;j <= 9;j++)
+		for (j = 0; j <= 9; j++)
 		{
-			product = i*j;
+			product = i * j;
 			if (j < 9)
 			{
 				printf("%2d, ", product);
 			}
-			else 
+			else
 				printf("%2d", product);
 		}
 		printf("\n");
