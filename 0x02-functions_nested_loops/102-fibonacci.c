58dd86d3e5811d73ddb5a980a481cd9f4ca9c4fe
gabaya <gabayaconsult@gmail.com>
2022-09-15 11:09:25 +0000

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-
-/*main- program entry point
+/**
+ * main - Entry point
  *
- *@void: no input
- * Return: 0 on success, 1 otherwise on failure
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printFibonacci(50);
 	return (0);
 }
-
-/*printFibonacci-recursively print first 50 fibonacci numbers
+/**
+ * printFibonacci-recursively print first 50 fibonacci numbers
  *
  *@n: number of fibonacci digits
  *Return: fibonacci numbes
@@ -28,7 +27,7 @@ void printFibonacci(int n)
 	if (n < 1)
 		return;
 	printf("%li, ", fib1);
-	for (i = 2; i <=n ; i++)
+	for (i = 2; i <= n; i++)
 	{
 		if (i == n)
 		{
