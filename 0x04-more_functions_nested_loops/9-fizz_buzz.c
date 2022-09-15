7558a7ed47131662c7aae503f0771e7430d1a01e
gabaya <gabayaconsult@gmail.com>
2022-09-15 22:03:49 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
