de3e9653916f9fa860b5b8bd06e14ecb433c81ac
gabaya <gabayaconsult@gmail.com>
2022-09-15 02:15:17 +0000

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
-/* print_to_98 - Print numbers from the input to 98
- *
- * @n: start number
- * Return:Void ,just print numbers
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * input number prints to 98
+ * @n: number input
+ * Return: Always 0 (Success)
  */
 void print_to_98(int n)
 {
