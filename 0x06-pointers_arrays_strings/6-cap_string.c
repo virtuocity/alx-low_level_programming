799dec3b98a0d6ee6b4ba5087c72a412b83a2fa9
gabaya <gabayaconsult@gmail.com>
2022-09-22 09:27:26 +0000

    Capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @s: string input
+ * Return: String with all words capitalized
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == ';' || s[i] == '\n' || s[i] == '\t'
+		|| s[i] == ',' || s[i] == '.' || s[i] == '!' || s[i] == '?'
+		|| s[i] == '"' || s[i] == '(' || s[i] == ')' ||
+		s[i] == '{' || s[i] == '}')
+		if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		s[i + 1] = s[i + 1] - 32;
+	i++;
+	}
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
+	return (s);
+}
