13c24269309c495eb2707ca629fe7521c51e1005
gabaya <gabayaconsult@gmail.com>
2022-09-22 06:32:43 +0000

    Strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,32 +10,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, lencat;
+	int i, j;
 
-	i = 0;
-
-	while (*(dest + i) != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		i++;
 	}
 
-	j = 0;
-	while (*(src + j) != '\0')
-	{
-		j++;
-	}
-	if (n < 100)
-	{
-		lencat = i + n;
-	}
-	else
-		lencat = 1 + 100;
-	j = 0;
-	while (i < lencat)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
-		j++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
