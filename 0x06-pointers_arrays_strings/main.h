f8e7940e4648b09eef3890cd8b5df92d5b822538
gabaya <gabayaconsult@gmail.com>
2022-09-22 06:44:32 +0000

    strncpy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
