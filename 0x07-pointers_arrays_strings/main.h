6bb9c6cb5c6a6c556b95fe35675d13db2cd38c2b
gabaya <gabayaconsult@gmail.com>
2022-09-27 17:24:55 +0000

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
