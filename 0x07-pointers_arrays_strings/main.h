ae487e0a8b4182b346d12d6d3121d8b50a46fab5
gabaya <gabayaconsult@gmail.com>
2022-09-27 19:29:40 +0000

    strpbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 #include <stdio.h>
 
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
