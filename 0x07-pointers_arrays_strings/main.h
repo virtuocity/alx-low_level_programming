dff416d0a91981784e4c7a36f2fc240aec42d676
gabaya <gabayaconsult@gmail.com>
2022-09-27 20:00:39 +0000

    Set string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+void set_string(char **s, char *to);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
