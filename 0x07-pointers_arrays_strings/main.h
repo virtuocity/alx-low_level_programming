85d09bd0349c0c26210b0c214ef071921b86a001
gabaya <gabayaconsult@gmail.com>
2022-09-27 18:51:33 +0000

    strspn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(char c);
