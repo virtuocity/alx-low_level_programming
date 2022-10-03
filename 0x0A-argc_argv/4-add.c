502cc3d8d4e2af56edb2fc909b9b52acff7e1498
gabaya <gabayaconsult@gmail.com>
2022-10-03 08:08:16 +0000

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 /**
@@ -9,17 +10,26 @@
   */
 int main(int argc, char *argv[])
 {
-	int i, j, product;
+	int i, j;
+	long int sum;
 
-	if (argc != 3)
+	sum = 0;
+	if (argc > 1)
 	{
-		printf("Error\n");
-		return (1);
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!(isdigit(argv[i][j])))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
 	}
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
-	product = i * j;
-	printf("%d\n", product);
+	printf("%ld\n", sum);
 	return (0);
 }
 
