05ea6d3632d2bdcd4b135ac9ca94b0bcddcb6a6e
Virtuocity <12395797+virtuocity@users.noreply.github.com>
2022-10-05 12:30:28 +0300

    Add files via upload

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *_strpbrk - searches a string for any of a set of bytes
+ *
+ *@s: string to be searched
+ *@accept: string to be matched
+ *Return: string matched
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+	char *add = NULL;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[j] == accept[i])
+			{
+				if (add == NULL)
+					add = &(s[j]);
+				if (&(s[j]) < add)
+					add = &(s[j]);
+			}
+		}
+	}
+	return (add);
+}
