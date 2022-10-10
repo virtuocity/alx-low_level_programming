d7a180c90949df8cbd9dc9ff6fde5bd9ffc83a42
gabaya <gabayaconsult@gmail.com>
2022-10-10 10:14:36 +0000

    2D Array dynamic memory allocation

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+  *alloc_grid - returns a pointer to a 2D array of integers
+  *
+  *@width: width,num of columns of array
+  *@height: height,num of rows of array
+  *Return: Pointer to 2D array
+  */
+int **alloc_grid(int width, int height)
+{
+	int **matrix;
+	int i, w, h;
+	int *p;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	matrix = (int **)malloc(height * sizeof(int *));
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		*(matrix + i) = (int *)malloc(width * sizeof(int *));
+		if (*(matrix + i) == NULL)
+		{
+			for (i = 0; i < height; i++)
+			{
+				p = matrix[i];
+				free(p);
+			}
+			free(matrix);
+			return (NULL);
+		}
+	}
+	for (h = 0; h < height; h++)
+	{
+		for (w = 0; w < height; w++)
+		{
+			matrix[h][w] = 0;
+		}
+	}
+	return (matrix);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void print_grid(int **grid, int width, int height);
+int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
