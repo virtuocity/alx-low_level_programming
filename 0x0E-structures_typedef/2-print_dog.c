7aa8cd6eaae1bc7a373ab55f1947febc78816908
gabaya <gabayaconsult@gmail.com>
2022-10-11 09:48:56 +0000

    Print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+  *print_dog-print dog record
+  *
+  *@d: pointer to struct dog
+  */
+void print_dog(struct dog *d)
+{
+	if(d != NULL)
+	{
+
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		printf("Name: %s\n",d->name);
+		if (d->age == 0)
+			printf("Age: (nil)");
+		printf("Age: %f\n", d->age);
+		if(d->owner == NULL)
+			printf("Owner: (nil)");
+		printf("Owner: %s\n", d->owner);
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,6 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif/* MAIN_H */
