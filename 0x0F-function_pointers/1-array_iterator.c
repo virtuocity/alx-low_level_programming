7c7793fdadecafaaddbb81aa15ca5b2e6f673c69
gabaya <gabayaconsult@gmail.com>
2022-10-15 12:41:13 +0000

    Function pointer to print array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+  *array_iterator - iterates an array
+  *
+  *@array:array itself
+  *@size: size of array
+  *@action: pointer to the required function
+  *Return; void
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action != NULL && array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+
+		action(array[i]);
+		}
+	}
+}
