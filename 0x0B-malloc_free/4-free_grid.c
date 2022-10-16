d34aa67f1332ccac5fe42dde199379e4fb682b62
gabaya <gabayaconsult@gmail.com>
2022-10-16 14:33:20 +0000

    Free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *free_grid - Deallocate memory for 2D int grid
+ *
+ * @grid: matrix of integers 
+ * @height: height of matrix 
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	int *p;
+
+	for (i = 0; i < height; i++)
+	{
+		p = grid[i];
+		free(p);
+	}
+	free(grid);
+}
