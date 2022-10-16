f256f62f3826ec64f0a88a59ac0e6d814db759c2
gabaya <gabayaconsult@gmail.com>
2022-10-16 14:21:56 +0000

    malloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		*(matrix + i) = (int *)malloc(width * sizeof(int *));
+		*(matrix + i) = (int *)malloc(width * sizeof(int));
 		if (*(matrix + i) == NULL)
 		{
 			for (i = 0; i < height; i++)
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -10,5 +10,6 @@ char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
+void free_grid(int **grid, int height);
 
 #endif /* MAIN_H */
