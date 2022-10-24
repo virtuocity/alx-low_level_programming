e12398ca3c359faaf2ac2a8e3207610bfd678a39
gabaya <gabayaconsult@gmail.com>
2022-10-24 17:19:15 +0000

    Print nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+  *print_list - prints all the elements of a list_t list
+  *
+  *@h: linked list
+  *Return: the number of nodes
+  */
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+			h = h->next;
+			i++;
+		}
+		else
+		{
+			printf("[%d] %s", h->len, h->str);
+			i++;
+			h = h->next;
+			printf("\n");
+		}
+	}
+	return (i);
+}
+
