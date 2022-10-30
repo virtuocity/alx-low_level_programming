593350ae67d682aea49bc45c8372220c7745b1b7
gabaya <gabayaconsult@gmail.com>
2022-10-30 07:16:20 +0000

    Print nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  *print_listint- print number of nodes in a SLL
+  *
+  *@h: pointer to headmof list
+  *Return: number of nodes
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	int nodes;
+
+	nodes = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,6 +19,9 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+int _putchar(char c);
+
 #endif /* LISTS_H */
 
 
