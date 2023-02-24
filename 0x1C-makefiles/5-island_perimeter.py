0c269b3fe9f6f1c9204ad5310df9f708141436d9
Buno Woche <bunogache@gmail.com>
2023-02-24 20:46:33 +0300

    added adjacency check

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,6 +14,34 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if grid[i][j] is 1:
-                perimeter += 1
+            if grid[i][j] == 1:
+                if i - 1 < 0:
+                    perimeter += 1
+                else:
+                    try:
+                        if grid[i - 1][j] == 0 or i - 1 < 0:
+                            perimeter += 1
+                    except:
+                        pass
+                if i + 1 > len(grid) - 1:
+                    perimeter += 1
+                else:
+                    try:
+                        if grid[i + 1][j] == 0:
+                            perimeter += 1
+                    except:
+                        pass
+                if j + 1 > len(grid[i]) - 1:
+                    perimeter += 1
+                else:
+                    try:
+                        if grid[i][j + 1] == 0:
+                            perimeter += 1
+                    except:
+                        pass
+                try:
+                    if grid[i][j- 1] == 0 or j - 1 < 0:
+                        perimeter += 1
+                except:
+                    pass
     return (perimeter)
