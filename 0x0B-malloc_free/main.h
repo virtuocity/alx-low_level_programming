4c416436d66055ab24930eb99947fdb48745529a
Buno Woche <bunogache@gmail.com>
2023-03-07 17:18:52 +0300

    concatenate args

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,5 +11,6 @@ char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 
 #endif /* MAIN_H */
