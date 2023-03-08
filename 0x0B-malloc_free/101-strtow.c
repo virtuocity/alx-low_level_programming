7412061baf0100e49a8d6adf20e8080a728f31e8
gabaya <gabayaconsult@gmail.com>
2023-03-08 13:56:15 +0000

    string to words consitions for counting words modified

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -21,7 +21,7 @@ char **strtow(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		/* count number of words */
-		if ((str[i] != ' ' && str[i + 1] != ' ') || (str[i + 1] == '\0'))
+		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
 		{
 			lenw++;
 		}
