7fff70f8cdaf15674b744630aea10a264686e59c
gabaya <gabayaconsult@gmail.com>
2023-03-08 13:48:07 +0000

    strings to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,22 +1,68 @@
 #include "main.h"
 
 /**
+  *strtow - print words in a string
   *
-  *
-  *
+  *@str: string to parse
+  *Return: pointer to pointer with words
   */
 char **strtow(char *str)
 {
 	char **c;
-	int i, j;
+	char *p;
+	int i, j, k, m, lenw, word, count;
 
-	if (str == NULL || str == "")
+	lenw = 0;
+	j = 0;
+	m = 0;
+
+	if (*str == '\0' || str == NULL)
 		return (NULL);
-	
-	for (i = 0; (str + i) != '\0'; i++)
-	;
-	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		/* count number of words */
+		if ((str[i] != ' ' && str[i + 1] != ' ') || (str[i + 1] == '\0'))
+		{
+			lenw++;
+		}
+	}
+	c = (char **)malloc((lenw + 1) * sizeof(char *));
+	if (c == NULL)
+		return (NULL);
+	for (word = 0; str[word] && j <= lenw; word++)
+	{
+		count = 0;
+		if (str[word] != ' ')
+		{
+			for (i = word; str[i] != '\0'; i++)
+			{
+				if (str[i] == ' ')
+					break;
+				count++;
+			}
+			*(c + j) = (char *)malloc((count + 1) * sizeof(char));
+			if (*(c + j) == NULL)
+			{
+				for (k = 0; k <= j; k++)
+				{
+					p = c[k];
+					free(p);
+				}
+				free(c);
+				return (NULL);
+			}
+			for (m = 0; word < i; word++)
+			{
+				c[j][m] = str[word];
+				m++;
+			}
+			c[j][m] = '\0';
+			j++;
+		}
 
+	}
+	c[j] = NULL;
+	return (c);
 
 }
 
