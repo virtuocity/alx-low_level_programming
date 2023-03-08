31dd427cc95ac098636ececf97a67c65613c6428
gabaya <gabayaconsult@gmail.com>
2023-03-08 10:38:41 +0000

    string to words partial solve

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *
+  *
+  *
+  */
+char **strtow(char *str)
+{
+	char **c;
+	int i, j;
+
+	if (str == NULL || str == "")
+		return (NULL);
+	
+	for (i = 0; (str + i) != '\0'; i++)
+	;
+	
+
+
+}
+
+
