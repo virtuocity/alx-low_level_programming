b40bfd17f3eafdb0612b6b31f8ed4a6e1584e887
gabaya <gabayaconsult@gmail.com>
2023-03-09 12:54:30 +0000

    stringn_concat length of n correction II

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,28 +26,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (l = 0; s2[l] != '\0'; l++)
 		;
-	if (l >= n)
+	if (n >= l)
 	{
 		new_n = l;
 	}
 	else
-	{
-		for (l = 0; l < n; l++)
-		{
-			new_n++;
-		}
-	}
-	c = (char *)malloc((i + l + 1) * sizeof(char));
+		new_n = n;
+	c = (char *)malloc((i + new_n + 1) * sizeof(char));
 	if (c == NULL)
 		return (NULL);
 	for (j = 0; s1[j] != '\0'; j++)
 	{
 		c[j] = s1[j];
 	}
-	for (k = 0; k < new_n; j++)
+	for (k = 0; k < new_n; k++)
 	{
 		c[j] = s2[k];
-		k++;
+		j++;
 	}
 	c[j] = '\0';
 	return (c);
