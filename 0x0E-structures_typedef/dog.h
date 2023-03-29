0089a71a54496884f4db206ea4367b8abef09ca4
gabaya <gabayaconsult@gmail.com>
2023-03-29 08:34:22 +0000

    struct dog correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,16 +12,16 @@
   *Description: name, age and owner of a dog
   */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
-void init_dog(struct dog *d, char *name, float age, char *owner);
+};
+/*void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+void free_dog(dog_t *d);*/
 
 #endif /* DOG_H */
