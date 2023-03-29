d7abef69e5d917d787d05668aa9c044e84071d38
gabaya <gabayaconsult@gmail.com>
2023-03-29 08:47:05 +0000

    print dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,9 +19,9 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 /**
-*void -  print_dog(struct dog *d);
-*typedef dog dog_t;
+*typedef - dog dog_t;
 *dog_t *new_dog(char *name, float age, char *owner);
 *void free_dog(dog_t *d);
 */
