33ecd507c862c05b552f2620d70890ec83593856
gabaya <gabayaconsult@gmail.com>
2023-04-11 16:18:46 +0000

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,17 +12,14 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL || size <= 0)
+	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-		{
-			printf("Found %d at index: %lu\n", value, i);
-			break;
-		}
+			return (i);
 	}
-	return (i);
+	return (-1);
 }
 
