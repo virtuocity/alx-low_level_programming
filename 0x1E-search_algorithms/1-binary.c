38f6d20927e1f74169fd3dc75eeff3a0197b487c
gabaya <gabayaconsult@gmail.com>
2023-04-13 08:58:07 +0000

    Binary search I

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+  *binary_search - search array using binary search algo
+  *
+  *@array: pointer to input array
+  *@size: num of elements in array sizeof(ayray)/sizeofarray[0]
+  *@value: search item
+  *Return: index of value which is an integer
+  */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t high_idx, mid_idx, low_idx;
+	
+	low_idx = 0;
+	high_idx =size - 1;
+	if (array == NULL)
+		return (-1);
+	for (low_idx = 0;low_idx < size;low_idx++)
+	{
+		printf("Searching in array: ");
+		for(; low_idx < high_idx; low_idx++)
+		{
+			printf("%d, ", array[low_idx]);
+		}
+		printf("%d\n", array[low_idx]);
+		mid_idx =(low_idx + high_idx)/2;
+		printf("Mid index is: %ld", mid_idx);
+		if (array[mid_idx]== value)
+		{
+			return (mid_idx);
+		}
+		if (array[mid_idx] < value)
+		{
+			low_idx = mid_idx + 1;
+		}
+		else
+			high_idx = mid_idx + 1;
+	}
+	return (-1);
+}
