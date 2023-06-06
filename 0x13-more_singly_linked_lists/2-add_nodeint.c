9a3171e89cceec767947185bc449358b0b6dbf0a
gabaya <gabayaconsult@gmail.com>
2023-06-06 12:25:42 +0000

    MoreSLL Add node at beginning

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+  *add_nodeint - add node at beginnning of SLL
+  *
+  *@head: head node of SLL
+  *@n: data
+  *Return: address of new element
+  */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	struct listint_s *new = NULL;
+	
+	new = (struct listint_s *)malloc(sizeof(struct listint_s));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
