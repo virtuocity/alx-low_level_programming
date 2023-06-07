3dd6fd465df260d1021635872d6b2b7258c478a5
gabaya <gabayaconsult@gmail.com>
2023-06-07 17:38:15 +0000

    add node end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,8 +9,24 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	struct listint_s *new = (listint_s *)malloc(sizeof(struct listint_s));
+	struct listint_s *new = NULL;
+	struct listint_s *temp = NULL;
 
+	new = (struct listint_s *)malloc(sizeof(struct listint_s));
 	if (new == NULL)
 		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL) 
+	{
+		*head = new;
+	}else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+	return (new);
+}
 	
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,7 +24,7 @@ size_t print_listint(const listint_t *h);
 int _putchar(char c);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
-
+void free_listint(listint_t *head);
 #endif /* LISTS_H */
 
 
