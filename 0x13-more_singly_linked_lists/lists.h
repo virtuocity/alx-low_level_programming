f5cc5c525721540c92b6ebf3d079a5dc05c3aa6d
gabaya <gabayaconsult@gmail.com>
2023-06-26 07:29:42 +0000

    fre list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -25,6 +25,7 @@ int _putchar(char c);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
 #endif /* LISTS_H */
 
 
