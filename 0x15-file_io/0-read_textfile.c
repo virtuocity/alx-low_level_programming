905b7733284559a7c80fb4e4d0fe5051557a8b6c
gabaya <gabayaconsult@gmail.com>
2023-11-27 16:54:37 +0000

    read file; git push

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+  *read_textfile - read a text file and output to stdout
+  *
+  *@filename:the file to read
+  *@letters: number of letters to read
+  * Return: the actual number of letters it could read and print
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fildes, size;
+	char *buf;
+
+	if (!filename)
+		return (0);
+	buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+		return (0);
+	fildes = open(filename, 0);
+	if (fildes == -1)
+		return(0);
+	size = write(STDOUT_FILENO, buf, read(fildes, buf, letters));
+	if (size == -1)
+		return (0);
+	close(fildes);
+	free(buf);
+	return(size);
+}
+
