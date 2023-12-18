c67f9897f92efdf820d02a183f45ba75b776278e
Buno Woche <bunogache@gmail.com>
2023-12-18 12:28:12 +0300

    set bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * @brief Set the bit object at index position to 0
+ * 
+ * @param n decimal number
+ * @param index index of bitto set to 1
+ * @return return 1 if done , -1 other wise
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+    unsigned long int mask, new_bits;
+
+    new_bits = 0;
+    mask = 1;
+    if (index > 63)
+        return (-1);
+    new_bits = *n & ~(mask << index);
+    *n = new_bits;
+    return (1);
+}
