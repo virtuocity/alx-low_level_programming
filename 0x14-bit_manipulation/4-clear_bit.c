8f7bf83ded729b98d0eacd517f950284431be6d8
Buno Woche <bunogache@gmail.com>
2023-12-18 13:49:34 +0300

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,7 @@
  * @param index index of bitto set to 1
  * @return return 1 if done , -1 other wise
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
     unsigned long int mask, new_bits;
 
