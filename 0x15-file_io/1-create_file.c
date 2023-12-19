ffac5400b7fcb9c597dd6f993b24a537ef3ee980
gabaya <gabayaconsult@gmail.com>
2023-12-19 11:44:13 +0000

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,32 +9,32 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, size, i;
-    char *buf;
+	int fd, size, i;
+	char *buf;
 
-    if (filename == NULL)
-        return (-1);
-    if (text_content == NULL)
-    /* create an empty file */
-    {
-        fd = open(filename, O_CREAT, 0600);
-        if (fd == -1)
-            return (-1);
-        return (1);
-    }
-    for(i = 0; text_content[i] != '\0'; i++)
-        ;
-    buf = malloc(i * sizeof(char));
-    if (buf == NULL)
-        return (-1);
-    /* create file */
-    fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-    if (fd == -1)
-        return (-1);
-    size = write(fd, text_content, i);
-    if (size == -1)
-        return (-1);
-    close(fd);
-    free(buf);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		/* create an empty file */
+	{
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+		return (1);
+	}
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	buf = malloc(i * sizeof(char));
+	if (buf == NULL)
+		return (-1);
+	/* create file */
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	size = write(fd, text_content, i);
+	if (size == -1)
+		return (-1);
+	close(fd);
+	free(buf);
+	return (1);
 }
