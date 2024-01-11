0b8ae255efb6fbd5e1504012471cdb8c17fd9546
gabaya <gabayaconsult@gmail.com>
2024-01-11 17:24:41 +0000

    insert node at index

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 void free_dlistint(dlistint_t *head);
