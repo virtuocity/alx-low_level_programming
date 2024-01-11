1d3db46c417ef2a55d9427cf041ac18aaf6a8661
gabaya <gabayaconsult@gmail.com>
2024-01-11 10:10:44 +0000

    Hotfix: case next node prev pointer to new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,23 +8,28 @@
   */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new = (dlistint_t *)malloc(sizeof(dlistint_t));
+	dlistint_t *new = NULL;
 
+
+	if (head == NULL)
+		return (NULL);
+	new = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	if (*head == NULL)
 	{
 		new->n = n;
-		new->next = NULL;
-		new->prev = *head;
+		new->next = *head;
+		new->prev = NULL;
 		*head = new;
+		return (new);
 	}
 	else
 	{
 		new->n = n;
+		(*head)->prev = new;
 		new->next = *head;
-		new->prev = (*head)->prev;
 		*head = new;
+		return (new);
 	}
-	return (new);
 }
