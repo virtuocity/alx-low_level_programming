0129d26bc1fc34e8b7d316b98d7153b988508103
gabaya <gabayaconsult@gmail.com>
2024-01-15 16:35:18 +0000

    dynamic lib of vaious ops

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * add - add two integers
+  *@a: first num
+  *@b: second number
+  *Return: a + b
+  */
+float div(int a, int b)
+{
+	float result = 0;
+
+	result = (float)a / b;
+	return (result);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * add - add two integers
+  *@a: first num
+  *@b: second number
+  *Return: a + b
+  */
+int sub(int a, int b)
+{
+	return (a - b);
+}
